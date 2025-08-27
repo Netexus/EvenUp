5a28becbfa2f57ddd491d88b2f0ad9037c043ec5
Oomass7 <loaizarodrigueztomas@gmail.com>
2025-08-26 20:35:58 -0500

    add: files for endpoints groups

---
+++ backend/src/models/expenseGroup.model.js
@@ -0,0 +1,36 @@
+const pool = require('../config/database');
+
+const ExpenseGroup = {
+  async getAll() {
+    const [rows] = await pool.query('SELECT * FROM expense_groups');
+    return rows;
+  },
+
+  async create(data) {
+    const { group_name, created_by, origin, destination, departure, trip_return, income_1, income_2} = data;
+    const [result] = await pool.query(
+      'INSERT INTO expense_groups (group_name, created_by, origin, destination, departure, trip_return, income_1, income_2) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+      [group_name, created_by, origin, destination, departure, trip_return, income_1, income_2]
+    );
+    const [rows] = await pool.query('SELECT * FROM expense_groups WHERE group_id = ?', [result.insertId]);
+    return rows[0];
+  },
+
+  async update(id, data) {
+    const { group_name, origin, destination, departure, trip_return, income_1, income_2 } = data;
+    const [result] = await pool.query(
+      'UPDATE expense_groups SET group_name = ?, origin = ?, destination = ?, departure = ?, trip_return = ?, income_1 = ?, income_2 = ? WHERE group_id = ?',
+      [group_name, origin, destination, departure, trip_return, income_1, income_2, id]
+    );
+    if (result.affectedRows === 0) return null;
+    const [rows] = await pool.query('SELECT * FROM expense_groups WHERE group_id = ?', [id]);
+    return rows[0];
+  },
+
+  async delete(id) {
+    const [result] = await pool.query('DELETE FROM expense_groups WHERE group_id = ?', [id]);
+    return result.affectedRows > 0;
+  }
+};
+
+module.exports = ExpenseGroup;
